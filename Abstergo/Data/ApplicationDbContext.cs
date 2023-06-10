b87e5b8163ff75b4d680e767f066f085f869bf59
the-prism <t.castonguay.gagnon@gmail.com>
2023-06-10 09:06:45 -0400

    Add Initial db context that works with sqlite

--- Abstergo/Data/ApplicationDbContext.cs
+++ Abstergo/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Abstergo.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Abstergo.Data
@@ -8,6 +10,17 @@ namespace Abstergo.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Favorite> Links => this.Set<Favorite>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IdentityRole>(entity => entity.Property(x => x.Name).HasMaxLength(255));
+            modelBuilder.Entity<IdentityRole>(entity => entity.Property(x => x.NormalizedName).HasMaxLength(255));
+            modelBuilder.Entity<IdentityRole>(entity => entity.Property(x => x.Id).HasMaxLength(255));
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
